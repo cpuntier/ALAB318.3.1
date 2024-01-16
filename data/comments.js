cba56b6bc4847118d617f55d48c790675b4b21e0
cpuntier <cpuntier@gmail.com>
2024-01-15 21:26:05 -0500

    post comments works

---
+++ data/comments.js
@@ -0,0 +1,12 @@
+const comments = [
+    {
+        id: 1,
+        userId:1,
+        postId:1,
+        body:"Wow I think this is really neat",
+    }
+]
+
+
+
+module.exports  = comments;
--- routes/comments.js
+++ routes/comments.js
@@ -14,8 +14,20 @@ router.route("/")
             },
         ];
 
-        res.json(comments,links)
-    })
 
+        res.json({comments,links})
+
+    }).post((req,res) => {
+        if (req.body.userId && req.body.postId && req.body.body) {
+            const comment = {
+              id: comments[comments.length - 1].id + 1,
+              userId: req.body.userId,
+              postId: req.body.postId,
+              body: req.body.body,
+            };
+            comments.push(comment);
+            res.json(comments[comments.length - 1]);
+          } else next(error(400, "Insufficient Data"));
+        });
 
 module.exports = router;
