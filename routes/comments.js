25ec0a4303f4e1d7a03926b6677a84ea9460774b
cpuntier <cpuntier@gmail.com>
2024-01-15 21:38:28 -0500

    get comment with userId works

--- routes/comments.js
+++ routes/comments.js
@@ -8,26 +8,55 @@ router.route("/")
     .get((req, res) => {
         const links = [
             {
-                href: "posts/:id",
+                href: "comments/:id",
                 rel: ":id",
                 type: "GET",
             },
         ];
 
 
-        res.json({comments,links})
+        res.json({ comments, links })
 
-    }).post((req,res) => {
+    }).post((req, res) => {
         if (req.body.userId && req.body.postId && req.body.body) {
             const comment = {
-              id: comments[comments.length - 1].id + 1,
-              userId: req.body.userId,
-              postId: req.body.postId,
-              body: req.body.body,
+                id: comments[comments.length - 1].id + 1,
+                userId: req.body.userId,
+                postId: req.body.postId,
+                body: req.body.body,
             };
             comments.push(comment);
             res.json(comments[comments.length - 1]);
-          } else next(error(400, "Insufficient Data"));
-        });
+        } else next(error(400, "Insufficient Data"));
+    });
 
+router.route("/:id")
+    .get((req, res,next) => {
+        // const links = [
+        //     {
+        //       href: `/${req.params.id}`,
+        //       rel: "",
+        //       type: "PATCH",
+        //     },
+        //     {
+        //       href: `/${req.params.id}`,
+        //       rel: "",
+        //       type: "DELETE",
+        //     },
+        //   ];
+
+
+
+
+        comment = comments.find((c) => c.id == req.params["id"])
+        console.log(comment);
+
+        if (comment) {
+
+            res.json({ comment });
+        } else {
+            next();
+        }
+
+    })
 module.exports = router;
